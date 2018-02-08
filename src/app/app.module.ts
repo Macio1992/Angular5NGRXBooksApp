3b485fd98f88f789669b43736da4c15f2df15ed9
Macio1992 <maix19bep@gmail.com>
2018-02-08 21:54:47 +0100

    ngrx router store configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+
+import { RouterModule } from '@angular/router';
 import { routes } from './routes';
+
+import { StoreModule } from '@ngrx/store';
+import { StoreRouterConnectingModule, RouterStateSerializer } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { CustomRouterStateSerializer } from './reducers/custom-router-state-serializer';
+
+import { environment } from '../environments/environment';
+import { reducers, metaReducers } from './reducers';
+
+import { AppComponent } from './app.component';
 import { ComponentsModule } from './theme/components/index';
 
 @NgModule({
@@ -16,9 +26,17 @@ import { ComponentsModule } from './theme/components/index';
     ComponentsModule,
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot(routes, {useHash: true})
+    RouterModule.forRoot(routes, {useHash: true}),
+    StoreModule.forRoot(reducers, { metaReducers }),
+    StoreRouterConnectingModule.forRoot({
+      stateKey: 'router',
+    }),
+    StoreDevtoolsModule.instrument({
+      name: 'NgRx Book Store DevTools',
+      logOnly: environment.production,
+    })
   ],
-  providers: [],
+  providers: [{ provide: RouterStateSerializer, useClass: CustomRouterStateSerializer }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
